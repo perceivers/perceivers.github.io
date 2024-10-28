f0a22ad4636e26a72ace381807356fb1fa08e72e
Luke Yi <luke@waye.com>
2024-10-28 14:59:00 +1300

    change from GTM to Google Analytics

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,11 +1,12 @@
-(function(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-KLZ7GF4L');
+// analytics.js
+(function() {
+  var gtagScript = document.createElement('script');
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MWT1X4KCMM';
+  gtagScript.async = true;
+  document.head.appendChild(gtagScript);
 
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-MWT1X4KCMM');
+})();
